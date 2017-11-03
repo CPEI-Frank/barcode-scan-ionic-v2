352086df2aa9ba608a7cc3ca34531df05c2c7e88
Héctor Menduíña Hermelo <kotehector@gmail.com>
2017-11-03 20:58:44 +0100

    base code ionic v2 app with barcode scan plugin

---
+++ .history/src/pages/home/home_20171003195508.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { BarcodeScanner, BarcodeScannerOptions } from '@ionic-native/barcode-scanner';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+	options: BarcodeScannerOptions;
+	results: {};
+
+  constructor(
+		private barcode: BarcodeScanner,
+		public navCtrl: NavController
+	) { }
+
+	async scanBarcode() {
+		this.results = await this.barcode.scan();
+		console.log(this.results);
+	}
+
+}
---
+++ .history/src/pages/home/home_20171003195627.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { BarcodeScanner, BarcodeScannerOptions } from '@ionic-native/barcode-scanner';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+	options: BarcodeScannerOptions;
+	results: {};
+
+  constructor(
+		private barcode: BarcodeScanner,
+		public navCtrl: NavController
+	) { }
+
+	async scanBarcode() {
+
+		this.options = {
+			prompt: 'Escanea un Código de barras para ver el resultado!';
+		}
+		this.results = await this.barcode.scan(this.options);
+		console.log(this.results);
+	}
+
+}
